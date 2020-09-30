d587c3e27a69c3d490134b8f5dc0be8785f336a6
Romanoli <inthewavves@gmail.com>
2020-09-30 21:11:10 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+    let result = '',
+        str = n.toString(); 
+        i = str.length - 1;
+        
+    while (i >= 0) {
+      result += str[i];
+      i--;
+    }
+    return parseInt(result);
+  }
